bf9769f547766b4d956bc84d5e27751e5dc2ec6a
bvnghiem <bvnghiem1012@gmail.com>
2020-10-04 23:16:42 +0700

    initialization web project

---
+++ SpringBoot/gau/toys-web/src/main/java/com/nghiem/gau/toys/web/ToysWebStarter.java
@@ -0,0 +1,12 @@
+package com.nghiem.gau.toys.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToysWebStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToysWebStarter.class, args);
+    }
+}
